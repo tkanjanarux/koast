8eebedfb6fbce5c1f0b5e29f2538d5d868c4da26
John Sintal <john.sintal@gmail.com>
2014-05-05 15:30:40 -0400

    JS/VB[client] added varun's changes to createResource

--- client/src/resource.js
+++ client/src/resource.js
@@ -214,7 +214,8 @@ angular.module('koast-resource', ['koast-user'])
     service.createResource = function (endpointHandle, body) {
       return post(endpointHandle, body)
         .then(function(result) {
-          console.log(result)
+          console.log(result);
+          return result;
         }, $log.error);
     };
 
