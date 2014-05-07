0bbb84d38b71517aea64f501bbcc53659ad6666d
Yuri Takhteyev <yuri@freewisdom.org>
2014-05-07 02:38:08 -0400

    Switching to using rangle-gulp and setting up karma tests.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,47 +1,32 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var beautify = require('gulp-beautify');
+var rg = require('rangle-gulp');
 
-var paths = {
-  clientScripts: ['client/app/**/*.js', 'client/app/*.js'],
-  serverScripts: ['server/app.js', 'server/lib/**/*.js']
-};
+gulp.task('karma', rg.karma({
+  files: [
+    // 3rd party
+    'client/bower_components/angular/angular.min.js',
+    'client/bower_components/angular-mocks/angular-mocks.js',
+    'client/bower_components/sinon-chai/lib/sinon-chai.js',
+    // Our code
+    'client/src/**/*.js'
+  ]
+}));
 
-var options = {};
+gulp.task('karma-watch', rg.karmaWatch({
+  // files: specify which folders
+  // karmaConf: specify which karma config file
+}));
 
-paths.allScripts = paths.clientScripts.concat(paths.serverScripts);
+gulp.task('lint', rg.jshint({
+  // files: specify which files
+}));
 
-options.beautify = {
-  braceStyle: "collapse",
-  breakChainedMethods: false,
-  e4x: false,
-  evalCode: false,
-  indentChar: " ",
-  indentLevel: 0,
-  indentSize: 2,
-  indentWithTabs: false,
-  jslintHappy: true,
-  keepArrayIndentation: false,
-  keepFunctionIndentation: false,
-  maxPreserveNewlines: 10,
-  preserveNewlines: true,
-  spaceBeforeConditional: true,
-  spaceInParen: false,
-  unescapeStrings: false,
-  wrapLineLength: 80
-};
+gulp.task('beautify', rg.beautify({
+  // files: specify which files
+}));
 
-
-gulp.task('lint', function() {
-  gulp.src(paths.allScripts)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
-gulp.task('beautify', function() {
-  gulp.src(paths.allScripts)
-    .pipe(beautify(options.beautify))
-    .pipe(gulp.dest('./public'))
-});
+gulp.task('dev', rg.nodemon({
+  onChange: ['lint'] // or ['lint', 'karma']
+}));
 
 gulp.task('default', ['lint']);
---
+++ client/src/user.test.js
@@ -0,0 +1,6 @@
+expect = chai.expect;
+describe('user server', function() {
+  it('user should get loaded', function () {
+    console.log('user');
+  });
+});
