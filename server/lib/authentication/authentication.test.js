72b3eaaa393dff62ac5e1b03df1e41126ea6ae4b
John Sintal <john.sintal@gmail.com>
2014-05-14 11:28:14 -0400

    JS[tests] testing saveUser in authentication, very basic

--- server/lib/authentication/authentication.test.js
+++ server/lib/authentication/authentication.test.js
@@ -1,5 +1,42 @@
-describe('Authentication', function () {
-  it('Fails', function() {
-    // throw new Error('Oops');
+'use strict';
+
+var expect = require('chai').expect;
+// var Q = require('q');
+// var _ = require('underscore');
+// var config = require('../config');
+// var dbUtils = require('../database/db-utils');
+var authentication = require('./authentication');
+
+var
+//mongooseMock = require('mongoose-mock'),
+//proxyquire = require('proxyquire'),
+chai = require('chai'),
+  expect = chai.expect,
+  sinon = require('sinon'),
+  sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+
+
+describe('Authentication', function() {
+  it('should save a user with an encrypted password', function(done) {
+
+    var callback = sinon.spy();
+
+    var name = 'someName';
+    var user = {
+      save: function(cb) {
+        cb(null, {
+          username: name
+        });
+      }
+    };
+
+    authentication.saveUser(user, 'somePassword')
+      .then(function(res) {
+        expect(res.username).to.equal(name);
+        //expect(res.password).to.not.be.undefined;
+
+        done();
+      });
   });
 });
---
+++ server/lib/authentication/password.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('chai').expect;
+// var Q = require('q');
+// var _ = require('underscore');
+// var config = require('../config');
+// var dbUtils = require('../database/db-utils');
+var password = require('./password');
+
+describe('Password', function() {
+  it('should reset the password with a new password (encrypted), and email the user of such change.', function() {
+    //expect(password.testMe()).to.equal(2);
+  });
+});
